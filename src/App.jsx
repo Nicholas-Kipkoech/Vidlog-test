74f84cda8f144124e40dd27d59abb7b12e4f69f5
kipkoech <nickey968@gmail.com>
2022-12-11 11:11:50 +0300

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
+
+function App() {
+  const [summary, setSummary] = useState("");
+  const [text, setText] = useState("");
+
+  const configuration = new Configuration({
+    apiKey: "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",
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const fetchSumamary = async () => {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `Summarize this for a second-grade student:\n\n:${text}`,
+      temperature: 0.7,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+    setSummary(response.data.choices[0].text);
+  };
+
+  return (
+    <div className="App">
+      <p className="title">
+        Text summarizer VidLogs Test using text-summarizer model
+      </p>
+
+      <div className="summary-area">
+        <div className="text-area-div">
+          <textarea
+            className="text-area"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter texts to summarize"
+          ></textarea>
+        </div>
+        <div className="text-area-div">
+          <div className="text-area2">{summary}</div>
+        </div>
+      </div>
+      <button className="btn-summary" onClick={() => fetchSumamary()}>
+        Summarize
+      </button>
+    </div>
+  );
+}
+
+export default App;
