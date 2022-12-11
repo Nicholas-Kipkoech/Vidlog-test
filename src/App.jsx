7241ca26ffd9e2ba3420cbcda81f3a02afcc96c6
kipkoech <nickey968@gmail.com>
2022-12-11 12:17:17 +0300

    change to prod

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [text, setText] = useState("");
 
   const configuration = new Configuration({
-    apiKey: import.meta.env.VITE_API_KEY,
+    apiKey: import.meta.env.PROD.VITE_API_KEY,
   });
 
   const openai = new OpenAIApi(configuration);
