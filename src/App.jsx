ddc80ffe7d44278a0008b4fbc59cd28d8ceb1894
kipkoech <nickey968@gmail.com>
2022-12-12 05:35:08 +0300

    chore(rname):renamed the env variables

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const fetchSumamary = async () => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `Summarize this for a second-grade student:\n\n:${text}`,
+      prompt: `Summarize this text:\n\n:${text}`,
       temperature: 0.7,
       max_tokens: 64,
       top_p: 1.0,
